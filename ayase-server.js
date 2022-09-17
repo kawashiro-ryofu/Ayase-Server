9a6d0e5e03515e7d4a3a2f06dcc2e7d9076ca72e
kawashiro-ryofu <unscientific_kappa@outlook.com>
2022-09-17 12:34:07 +0800

    （代码格式化+部分完善）

--- ayase-server.js
+++ ayase-server.js
@@ -8,8 +8,17 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
-const { version, license } = require('./package.json')
-const {Notice, Client, User} = require('./db.js')
+const
+{
+	version,
+	license
+} = require('./package.json')
+const
+{
+	Notice,
+	Client,
+	User
+} = require('./db.js')
 
 const tokenreg = /^[A-F,a-f,0-9]{16}$/
 
@@ -19,100 +28,170 @@ var app = express()
 app.use(express.static('static'))
 //  路由
 //      根  欢迎页
-app.get('/', (req, res)=>{
-    res.send('Ayase-Server Hello World')
+app.get('/', (req, res) =>
+{
+	res.send('Ayase-Server Hello World')
 })
 
 //      查询
 //      /query/:token/:obj
 //      token: 令牌（格式为字符串，内容为十六位十六进制数）
 //      obj: 查询对象。(LatestUpdateDate 最新更新时间；NoticesList 所有通知的列表)
-app.get('/query/:token/:obj', (req, res)=>{
+app.get('/query/:token/:obj', (req, res) =>
+{
 
-    //  obj:
-    //	   -1 -> (undefined, response 404)
-    //      0 -> LatestUpdateDate
-    //      1 -> NoticesList
-    var obj = -1
-    switch(req.params.obj){
-        case 'LatestUpdateDate': obj = 0; break;
-        case 'NoticesList': obj = 1; break;
-        default: obj = -1;
-    }
-    //  路由校验
-    if(tokenreg.test(req.params.token) && (obj != -1)){
+	//  obj:
+	//	   -1 -> (undefined, response 404)
+	//      0 -> LatestUpdateDate
+	//      1 -> NoticesList
+	var obj = -1
+	switch (req.params.obj)
+	{
+		case 'LatestUpdateDate':
+			obj = 0;
+			break;
+		case 'NoticesList':
+			obj = 1;
+			break;
+		default:
+			obj = -1;
+	}
+	//  路由校验
+	if (tokenreg.test(req.params.token) && (obj != -1))
+	{
 
-        //  验证Token
-        //  返回结果
-        //  需要数据库(MongoDB)
-        new Promise(function(resolve, reject){
-			Client.findOne({token: req.params.token}, (err, doc)=>{
-				if(err)	reject(err)
-				else{
-					if(doc == null || doc.token != req.params.token){
-						res.status(403).send('Access Denied')
-						return 0
-					}else{
-						resolve(doc)
+		//  验证Token
+		//  返回结果
+		//  需要数据库(MongoDB)
+		new Promise(function(resolve, reject)
+			{
+				Client.findOne(
+				{
+					token: req.params.token
+				}, (err, doc) =>
+				{
+					if (err) reject(err)
+					else
+					{
+						if (doc == null || doc.token != req
+							.params.token)
+						{
+							res.status(403)
+								.send('Access Denied')
+							return 0
+						}
+						else
+						{
+							resolve(doc)
+						}
 					}
-				}		
+				})
 			})
-        }).then(function(doc){
-			try{
-				let result = new Object()
-				if(obj){
-					let nUUIDList = Array.from(doc.notices)
-					result = {Notices: []}
-			                for(var a = 0; a < nUUIDList.length; a++){
-              	        			new Promise(function(resolve, reject){
-		                            		Notice.findOne({uuid: nUUIDList[a]}, function(err, Ndoc){
-			                                if(err) reject(err);
-        	        		                else resolve(Ndoc._doc)
-			                            })
-                			        }).then(function(Ndoc){
-	                        	    		switch(Ndoc.level){
-		                        	        	case 0: Ndoc.level = "D"; break;
-	                		        	        case 1: Ndoc.level = "C"; break;
-        	                        			case 2: Ndoc.level = "B"; break;
-				                                case 3: Ndoc.level = "A"; break;
-							}
-							result.Notices.push(Ndoc)
-							if(result.Notices.length == nUUIDList.length){
-								res.status(200)
-								res.json(result)
-							 }
-						}).catch(function(err){
-	                				throw err
-        	                		})
+			.then(function(doc)
+			{
+				try
+				{
+					let result = new Object()
+					if (obj)
+					{
+						let nUUIDList = Array.from(doc.notices)
+						result = {
+							Notices: []
+						}
+						for (var a = 0; a < nUUIDList.length; a++)
+						{
+							new Promise(function(resolve, reject)
+								{
+									Notice.findOne(
+									{
+										uuid: nUUIDList[
+											a]
+									}, function(err,
+										Ndoc)
+									{
+										if (err) reject(
+											err);
+										else resolve(
+											Ndoc._doc
+										)
+									})
+								})
+								.then(function(Ndoc)
+								{
+									switch (Ndoc.level)
+									{
+										case 0:
+											Ndoc.level = "D";
+											break;
+										case 1:
+											Ndoc.level = "C";
+											break;
+										case 2:
+											Ndoc.level = "B";
+											break;
+										case 3:
+											Ndoc.level = "A";
+											break;
+									}
+									result.Notices.push(Ndoc)
+									if (result.Notices.length ==
+										nUUIDList.length)
+									{
+										res.status(200)
+										res.json(result)
+									}
+								})
+								.catch(function(err)
+								{
+									throw err
+								})
+						}
 					}
-				}else{
-					result = {LatestUpdateDate: doc.NUT}
-			                res.status(200)
-			                res.json(result)
-			                return 0
+					else
+					{
+						result = {
+							LatestUpdateDate: doc.NUT
+						}
+						res.status(200)
+						res.json(result)
+						return 0
+					}
 				}
-			}catch(err){
-				res.status(500).send(err)
-                console.error(err)
+				catch (err)
+				{
+					res.status(500)
+						.send(err)
+					console.error(err)
+					return -1
+				}
+			})
+			.catch(function(err)
+			{
+				res.status(500)
+					.send(err)
+				console.error(err)
 				return -1
-			}
-        }).catch(function(err){
-			res.status(500).send(err)
-            console.error(err)
-			return -1
-        })
-        
-    }else if(tokenreg.test(req.params.token) && (obj == -1)){
-        res.status(404).send('Not Found')
-    }else res.status(403).send('Access Denied')
+			})
+
+	}
+	else if (tokenreg.test(req.params.token) && (obj == -1))
+	{
+		res.status(404)
+			.send('Not Found')
+	}
+	else res.status(403)
+		.send('Access Denied')
 })
 
 //      管理页
-app.get('/admin', (req,res)=>{
-    res.status(404).send('Not Yet')
+app.get('/admin', (req, res) =>
+{
+	res.status(404)
+		.send('Not Yet')
 })
 
 //		监听
-var server = app.listen(PORT, function(){
-    console.log(`Server Running On Port ${PORT}`)
+var server = app.listen(PORT, function()
+{
+	console.log(`Server Running On Port ${PORT}`)
 })
